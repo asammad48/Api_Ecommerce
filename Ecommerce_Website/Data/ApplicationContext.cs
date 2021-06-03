b43490ed2e255b691c975f667250626563c97117
Abdul Sammad <Abdul.1130563@talmeez.pk>
2021-06-03 17:11:10 +0500

    Thursday Work

--- Ecommerce_Website/Data/ApplicationContext.cs
+++ Ecommerce_Website/Data/ApplicationContext.cs
@@ -13,16 +13,12 @@ namespace Ecommerce_Website.Data
 
         }
         public DbSet<SubSubCategory> subSubCategories { get; set; }
-      public DbSet<SubCategory> subCategories { get; set; }
-      public DbSet<Category> Categories { get; set; }
-      public DbSet<Variants> variants { get; set; }
-      public DbSet<Product_Variants>  product_Variants { get; set; }
-       //DbSet<Product_Category>  product_Categories { get; set; }
-      public DbSet<Product>  products { get; set; }
-        public void OnModelCreating(ModelBuilder modelBuilder)
-        {
-           // modelBuilder.Entity<Product_Category>().HasMany().WithMany().WillCascadeOnDelete(false);
-        }
+        public DbSet<SubCategory> subCategories { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Variants> variants { get; set; }
+        public DbSet<Product_Variants>  product_Variants { get; set; }
+         //DbSet<Product_Category>  product_Categories { get; set; }
+        public DbSet<Product>  products { get; set; }
         public DbSet<Ecommerce_Website.Models.Category_subCategory> Category_subCategory { get; set; }
         public DbSet<Ecommerce_Website.Models.SubCategory_SubSubcategory> SubCategory_SubSubcategory { get; set; }
         public DbSet<Specification> specifications { get; set; }
